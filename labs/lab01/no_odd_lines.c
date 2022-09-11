72bf5fe65dd8a785d43f6a7f4c9b94359b65d492
Hoang Long <longpaus@gmail.com>
2022-09-11 16:02:11 +1000

    did no odd lines

--- labs/lab01/no_odd_lines.c
+++ labs/lab01/no_odd_lines.c
@@ -2,5 +2,13 @@
 #include <string.h>
 
 int main(void) {
+	char str[1024];
+	int linecount = 0;
+	while(fgets(str,1024,stdin) != NULL){
+		if(linecount % 2 == 0){
+			fputs(str,stdout);
+		}
+		linecount++;
+	}
 	return 0;
 }
