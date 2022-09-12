e3dc161b27d1219cb5e6c199f3c9eacc9b494c73
Hoang Long <longpaus@gmail.com>
2022-09-12 18:14:44 +1000

    did fininacci

--- labs/lab01/fibonacci.c
+++ labs/lab01/fibonacci.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 
 #define SERIES_MAX 30
+int fibonacciRecur(int num1,int num2,int count);
 
 int main(void) {
+    int num;
+    while(scanf("%d",&num) != EOF){
+        printf("%d\n",fibonacciRecur(0,1,num));
+    }
     return EXIT_SUCCESS;
+}
+
+int fibonacciRecur(int num1,int num2,int count){
+    if(count == 0)
+        return num1;
+    return fibonacciRecur(num2,num1+num2,count - 1);
 }
