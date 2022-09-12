79c588f9beb6e0864f88ac68203c88693d29357b
Hoang Long <longpaus@gmail.com>
2022-09-12 17:51:53 +1000

    updating

--- labs/lab01/arg_stats.c
+++ labs/lab01/arg_stats.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	int min = atoi(argv[1]),max = atoi(argv[1]);
+	int min = atoi(argv[1]), max = atoi(argv[1]);
 	int sum = 0, prod = 1;
 
-	for(int i = 1; i < argc; i++){
-		if(atoi(argv[i]) < min){
+	for (int i = 1; i < argc; i++) {
+		if (atoi(argv[i]) < min) {
 			min = atoi(argv[i]);
-		}
-		else if (atoi(argv[i]) > max){
+		} else if (atoi(argv[i]) > max) {
 			max = atoi(argv[i]);
 		}
 		sum += atoi(argv[i]);
 		prod *= atoi(argv[i]);
 	}
-	printf("MIN:  %d\n",min);
-	printf("MAX:  %d\n",max);
-	printf("SUM:  %d\n",sum);
-	printf("PROD: %d\n",prod);
-	printf("MEAN: %d\n",sum/(argc-1));
+	printf("MIN:  %d\n", min);
+	printf("MAX:  %d\n", max);
+	printf("SUM:  %d\n", sum);
+	printf("PROD: %d\n", prod);
+	printf("MEAN: %d\n", sum / (argc - 1));
 }
--- labs/lab01/no_odd_lines.c
+++ labs/lab01/no_odd_lines.c
@@ -3,10 +3,9 @@
 
 int main(void) {
 	char str[1024];
-	while(fgets(str,1024,stdin) != NULL){
-		if(strlen(str) % 2 == 0)
-			fputs(str,stdout);
+	while (fgets(str, 1024, stdin) != NULL) {
+		if (strlen(str) % 2 == 0)
+			fputs(str, stdout);
 	}
 	return 0;
-
 }
