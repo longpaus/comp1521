18eb5f645ef2e221ce6727dff8b43abdfc4c2258
Hoang Long <longpaus@gmail.com>
2022-09-12 17:48:19 +1000

    did arg_stats

--- labs/lab01/arg_stats.c
+++ labs/lab01/arg_stats.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	(void) argc, (void) argv; // keep the compiler quiet, should be removed
-	return 0;
+	int min = atoi(argv[1]),max = atoi(argv[1]);
+	int sum = 0, prod = 1;
+
+	for(int i = 1; i < argc; i++){
+		if(atoi(argv[i]) < min){
+			min = atoi(argv[i]);
+		}
+		else if (atoi(argv[i]) > max){
+			max = atoi(argv[i]);
+		}
+		sum += atoi(argv[i]);
+		prod *= atoi(argv[i]);
+	}
+	printf("MIN:  %d\n",min);
+	printf("MAX:  %d\n",max);
+	printf("SUM:  %d\n",sum);
+	printf("PROD: %d\n",prod);
+	printf("MEAN: %d\n",sum/(argc-1));
 }
