c1754289c8c2432cdeb3afb6fc42ec9f0c38f699
Hoang Long <longpaus@gmail.com>
2022-09-12 18:01:18 +1000

    fix collatz

--- labs/lab01/collatz.c
+++ labs/lab01/collatz.c
@@ -5,12 +5,11 @@ int main(int argc, char **argv)
 {
 	int num = atoi(argv[1]);
 	while(num != 1){
+		printf("%d\n",num);		
 		if(num % 2 != 0)
 			num = 3*num + 1;
 		else
 			num /= 2;
-		if(num != 1)
-			printf("%d\n",num);	
 	}
 	printf("1\n");	
 	return EXIT_SUCCESS;
