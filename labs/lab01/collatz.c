2d40d30741f59524d9fb17518c9a847cb4c6f296
Hoang Long <longpaus@gmail.com>
2022-09-12 18:07:00 +1000

    redid collatz with recurrsion

--- labs/lab01/collatz.c
+++ labs/lab01/collatz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
+int collatzRecur(int num);
+
+int main(int argc, char **argv) {
 	int num = atoi(argv[1]);
-	while(num != 1){
-		printf("%d\n",num);		
-		if(num % 2 != 0)
-			num = 3*num + 1;
-		else
-			num /= 2;
-	}
-	printf("1\n");	
+	collatzRecur(num);
 	return EXIT_SUCCESS;
+}
+
+int collatzRecur(int num) {
+	printf("%d\n", num);
+	if (num == 1)
+		return 1;
+	return (num % 2 == 0) ? collatzRecur(num / 2) : collatzRecur(3 * num + 1);
 }
