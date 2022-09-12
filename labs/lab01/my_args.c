80f431597dd77af3d4bf408f3cbaf742561c4241
Hoang Long <longpaus@gmail.com>
2022-09-12 20:32:31 +1000

    did my_args

--- labs/lab01/my_args.c
+++ labs/lab01/my_args.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
+#define QUOTE '"'
 int main(int argc, char **argv) {
-	(void) argc, (void) argv; // keep the compiler quiet, should be removed
+	printf("Program name: %s\n",argv[0]);
+	if(argc - 1 != 0)
+		printf("There are %d arguments:\n",argc-1);
+	else{
+		printf("There are no other arguments\n");
+		return 0;
+	}
+	for(int i = 1;i < argc;i++){
+		printf("	Argument %d is %c%s%c\n",i,QUOTE,argv[i],QUOTE);
+	}
 	return 0;
 }
