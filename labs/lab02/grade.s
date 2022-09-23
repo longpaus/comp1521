728707a11a1c52d0e28f4af18b3a6b41a5bdae75
Hoang Long <longpaus@gmail.com>
2022-09-23 11:25:05 +1000

    did grade.s

--- labs/lab02/grade.s
+++ labs/lab02/grade.s
@@ -1,37 +1,65 @@
 # read a mark and print the corresponding UNSW grade
-#
+
 # Before starting work on this task, make sure you set your tab-width to 8!
 # It is also suggested to indent with tabs only.
-#
+
 # YOUR-NAME-HERE, DD/MM/YYYY
 
-#![tabsize(8)]
+# ![tabsize(8)]
 
 main:
-	la	$a0, prompt	# printf("Enter a mark: ");
-	li	$v0, 4
-	syscall
+    la      $a0,    prompt          # printf("Enter a mark: ");
+    li      $v0,    4
+    syscall 
 
-	li	$v0, 5		# scanf("%d", mark);
-	syscall
+    li      $v0,    5               # scanf("%d", mark);
+    syscall 
+    blt     $v0,    50,     fail    # if input < 50 goto fail
+    blt     $v0,    65,     pass    # if input < 65 goto pass
+    blt     $v0,    75,     credit
+    blt     $v0,    85,     dist
+    la      $a0,    hd
+    li      $v0,    4
+    syscall 
+    li      $v0,    0
+    jr      $ra
 
-	la	$a0, fl		# printf("FL\n");
-	li	$v0, 4
-	syscall
+fail:
+    la      $a0,    fl
+    li      $v0,    4
+    syscall 
+    li      $v0,    0
+    jr      $ra
 
-	li	$v0, 0
-	jr	$ra		# return 0
+pass:
+    la      $a0,    ps
+    li      $v0,    4
+    syscall 
+    li      $v0,    0
+    jr      $ra
+credit:
+    la      $a0,    cr
+    li      $v0,    4
+    syscall 
+    li      $v0,    0
+    jr      $ra
+dist:
+    la      $a0,    dn
+    li      $v0,    4
+    syscall 
+    li      $v0,    0
+    jr      $ra
 
-	.data
+.data
 prompt:
-	.asciiz "Enter a mark: "
+.asciiz "Enter a mark: "
 fl:
-	.asciiz "FL\n"
+.asciiz "FL\n"
 ps:
-	.asciiz "PS\n"
+.asciiz "PS\n"
 cr:
-	.asciiz "CR\n"
+.asciiz "CR\n"
 dn:
-	.asciiz "DN\n"
+.asciiz "DN\n"
 hd:
-	.asciiz "HD\n"
+.asciiz "HD\n"
