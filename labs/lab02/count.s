7fd0722519058ea3806097148121da2bdd808775
Hoang Long <longpaus@gmail.com>
2022-09-25 15:28:15 +1000

    did count

--- labs/lab02/count.s
+++ labs/lab02/count.s
@@ -1,32 +1,45 @@
 # read a number n and print the integers 1..n one per line
-#
+
 # Before starting work on this task, make sure you set your tab-width to 8!
 # It is also suggested to indent with tabs only.
-#
+
 # YOUR-NAME-HERE, DD/MM/YYYY
 
-#![tabsize(8)]
 
-main:                 		# int main(void)
-	la	$a0, prompt	# printf("Enter a number: ");
-	li	$v0, 4
-	syscall
+# ![tabsize(8)]
 
-	li	$v0, 5		# scanf("%d", number);
-	syscall
+main:                               # int main(void)
+    la      $a0,    prompt          # printf("Enter a number: ");
+    li      $v0,    4
+    syscall 
 
-	li	$a0, 42		# printf("%d", 42);
-	li	$v0, 1
-	syscall
+    li      $v0,    5               # scanf("%d", number);
+    syscall 
 
-	li	$a0, '\n'	# printf("%c", '\n');
-	li	$v0, 11
-	syscall
+    move    $s0,    $v0             # s0 - number
+
+    li      $t1,    1               # t1 - i
+loop:
+    bgt     $t1,    $s0,    end
+
+    li      $v0,    1               # print_int
+    move    $a0,    $t1             # print(i)
+    syscall 
+
+    li      $v0,    11              # print_char
+    li      $a0,    '\n'            # printf("%c", '\n');
+    syscall 
+
+    addi    $t1,    $t1,    1       # add the counter by 1
+    b       loop                    # branch to loop
+
+
+
 
 end:
-	li	$v0, 0
-	jr	$ra		# return 0
+    li      $v0,    0
+    jr      $ra                     # return 0
 
-	.data
+.data
 prompt:
-	.asciiz "Enter a number: "
+.asciiz "Enter a number: "
