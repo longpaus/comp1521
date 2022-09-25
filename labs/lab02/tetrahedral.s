b7f8b5d70f9fc656a86f43f3772722b7fd61be5f
Hoang Long <longpaus@gmail.com>
2022-09-26 00:00:37 +1000

    tedrahedral draft

--- labs/lab02/tetrahedral.s
+++ labs/lab02/tetrahedral.s
@@ -8,6 +8,13 @@
 
 #![tabsize(8)]
 
+#values:
+#t0 = how_many
+#t1 = n
+#t2 = total
+#t3 = j
+#t4 = i
+
 main:				# int main(void) {
 
 	la	$a0, prompt	# printf("Enter how many: ");
@@ -17,13 +24,45 @@ main:				# int main(void) {
 	li	$v0, 5		# scanf("%d", number);
 	syscall
 
-	li	$a0, 42		# printf("%d", 42);
-	li	$v0, 1
-	syscall
+	move	$t0,$v0 	#t0 = how_many
+	li	$t1,1		# t1 = 1 - n
 
+loop1Top:
+	bgt 	$t1,$t0,end	#if n > how_many goto end
+	li	$t2,0		#t2 = 0 - total
+	li	$t3,1		#t3 = 1 - j
+
+	ble	$t3,$t1,loop2	#if j <= n then goto loop2
+
+loop1Bottom:
+	li	$v0,1
+	move	$a0,$t2
+	syscall
 	li	$a0, '\n'	# printf("%c", '\n');
 	li	$v0, 11
 	syscall
+	addi	$t1,$t1,1	#n++
+	b	loop1Top	# branch to loop1Top
+	
+
+loop2Top:
+	bgt	$t3,$t1,loop1Bottom	#if j > n goto loop1Bottom
+	li	$t4,1		#t4 = 1 - i
+	ble	$t4,$t3,loop3	#if i <+ j goto loop3
+	b	loop2Bottom	# branch to loop2Bottom
+	
+
+loop2Bottom:
+	addi	$t3,$t3,1	#j++
+	b	loop2Top	# branch to loop2Top
+	
+
+loop3:
+	bgt	$t4,$t3,loop2Bottom	#if i > j goto loop2Bottom
+	addu	$t2,$t2,$t4	#total += i
+	addi	$t4,$t4,1	#i++
+
+
 
 end:
 	li	$v0, 0
