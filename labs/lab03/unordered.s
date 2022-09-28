948d490a2ad38dd9392f863bd3e29b8e1dee4cd8
Hoang Long <longpaus@gmail.com>
2022-09-28 22:24:36 +1000

    think i did unordered

--- labs/lab03/unordered.s
+++ labs/lab03/unordered.s
@@ -11,7 +11,10 @@ main:
 	#  - $t0: int i
 	#  - $t1: temporary result
 	#  - $t2: temporary result
+	# t3 = numbers[i]
+	# t4 = numbers[i + 1]
 	#  TODO: add your registers here
+	li	$t5,0				#  - $t5 = 0 : swapped
 
 scan_loop__init:
 	li	$t0, 0				# i = 0;
@@ -31,18 +34,51 @@ scan_loop__body:
 	j	scan_loop__cond			# }
 scan_loop__end:
 
-	# TODO: add your code here!
+	b	check_loop_init			# branch to check_loop_init
+	
 
-	li	$v0, 1				# syscall 1: print_int
-	li	$a0, 42 			#
-	syscall					# printf("%d", 42)
+	# li	$v0, 1				# syscall 1: print_int
+	# li	$a0, 42 			#
+	# syscall					# printf("%d", 42)
 
-	li	$v0, 11				# syscall 11: print_char
-	li	$a0, '\n'			#
-	syscall					# printf("%c", '\n');
+	# li	$v0, 11				# syscall 11: print_char
+	# li	$a0, '\n'			#
+	# syscall					# printf("%c", '\n');
 
-	li	$v0, 0
-	jr	$ra				# return 0;
+	# li	$v0, 0
+	# jr	$ra				# return 0;
+
+check_loop_init:
+	li	$t0, 0				# i = 0;
+check_loop:
+	bge	$t0,8,end			#if i > 8 goto end (8 instead of 9 since we are using i + 1)
+
+	#getting numbers[i]
+	mul	$t1,$t0,4			# the amount of memory i is at
+	la	$t2,numbers
+	add	$t2,$t2,$t1			# t2 = location of the integer i
+	lw	$t3,0($t2)			# t3 = numbers[i]
+
+	#getting numbers[i + 1]
+	addi	$t1,$t0,1			# t1 = i + 1
+	mul	$t1,$t1,4
+	la	$t2,numbers
+	add	$t2,$t2,$t1
+	lw	$t4,0($t2)			# t4 = numbers[i + 1]
+
+	addi	$t0,$t0,1			#i++
+
+	ble	$t3, $t4, check_loop		# if $t3 <= $t4 then check_loop
+
+	li	$t5,1				# t5 = 1
+	b	end				# branch to end
+
+end:	
+	move 	$a0,$t5
+	li	$v0,1
+	syscall
+	li $v0, 0
+    	jr $ra
 
 	.data
 numbers:
