6a0d9e6323378a1628474fbbb8fb643d2738c396
Hoang Long <longpaus@gmail.com>
2022-09-29 23:08:46 +1000

    think did bubblesort.s

--- labs/lab03/bubblesort.s
+++ labs/lab03/bubblesort.s
@@ -29,9 +29,52 @@ scan_loop__body:
 	addi	$t0, $t0, 1			#   i++;
 	b	scan_loop__cond			# }
 scan_loop__end:
+	li	$t0,1				# t0 = swapped
+	b	loop1				# branch to loop1
+	
+loop1:
+	beq	$t0, 0, print_loop__init	# if swapped == 0 end loop
+	li	$t0,0				#swapped = 0
+	li	$t1,1				#i = 1
+	b	loop2				# branch to loop2
 
-	# TODO: add your code here!
+loop2:
+	bge	$t1,ARRAY_LEN,loop1		# if i >= arrayLen goto loop1
+	#get numbers[i]
+	mul	$t2,$t1,4
+	la	$t3,numbers
+	add	$t3,$t3,$t2			# t3 = memory location numbers[i]
+	lw	$t6, 0($t3)			# t6 = numbers[i]
 
+	#get numbers[i - 1]
+	addi	$t2,$t1,-1			# t2 = i - 1
+	mul	$t2,$t2,4
+	la	$t4,numbers
+	add	$t4,$t4,$t2			# t4 = memory loaction of numbers[i - 1]
+	lw	$t7,0($t4)			# t7 = numbers[i - 1]
+
+	blt	$t6, $t7, swap			# if numbers[i] < nunbers[i - 1] goto swap	
+	b	loop2_increase_count		# branch to loop2_increase_count
+	
+
+swap:
+	sw	$t6,($t4)			# numbers[i - 1] = x
+	sw	$t7,($t3)			# nunbers[i] = y
+	li	$t0,1				#swapped = 1
+	b	loop2_increase_count		# branch to loop2_increase_count
+	
+
+
+loop2_increase_count:
+	addi	$t1,$t1,1			# i++
+	b	loop2				# branch to loop2
+
+	
+	
+
+
+
+	
 
 print_loop__init:
 	li	$t0, 0				# i = 0
