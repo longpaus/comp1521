6d7ce03ef762b7f97519a9f09d136d99fc3f35ae
Hoang Long <longpaus@gmail.com>
2022-10-02 19:48:50 +1100

    did not13.s

--- tests/test03/not13.s
+++ tests/test03/not13.s
@@ -1,20 +1,35 @@
 main:
-	li	$v0, 5		# scanf("%d", &x);
-	syscall			#
+	li	$v0, 5			# scanf("%d", &x);
+	syscall				#
 	move	$t0, $v0
 
-	li	$v0, 5		# scanf("%d", &y);
-	syscall			#
+	li	$v0, 5			# scanf("%d", &y);
+	syscall				#
 	move	$t1, $v0
 
-	li	$a0, 42		# printf("%d\n", 42);
+	addi	$t0,$t0.1		# i = x + 1
+
+loop:
+	bge	$t0,$t1,end		# if i >= y goto end
+	bne	$t0, 13, print		# if $t0 != $t1 then print
+	b	increaseCount		# branch to increaseCount
+	
+	
+	
+print:
+	move	$a0, $t0		# printf("%d\n", i);
 	li	$v0, 1
 	syscall	
 
-	li	$a0, '\n'	# printf("%c", '\n');
+	li	$a0, '\n'		# printf("%c", '\n');
 	li	$v0, 11
 	syscall	
-
+	b	increaseCount		# branch to increaseCount
+	
+increaseCount:
+	addi	$t0,$t0,1		#i++
+	b	loop			# branch to loop
+	
 end:
-	li	$v0, 0         # return 0
+	li	$v0, 0        		 # return 0
 	jr	$ra
