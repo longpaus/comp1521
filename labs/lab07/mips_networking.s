99a7d43ab9f085df9ff6b9a6918d933b60b97570
Hoang Long <longpaus@gmail.com>
2022-10-30 17:20:07 +1100

    did networeking

--- labs/lab07/mips_networking.s
+++ labs/lab07/mips_networking.s
@@ -6,17 +6,40 @@ main:
 	# Locals:
 	#	- $t0: int network_bytes
 	#	- $t1: int computer_bytes
-	#	- Add your registers here!
+	#	- $t2 - int byte_mask
 
 
 	li	$v0, 5		# scanf("%d", &x);
 	syscall
+	move	$t0,$v0
+	li	$t1,0
+	li	$t2,0xFF
+
+	and 	$t3,$t0,$t2	# t3 = network_bytes & byte_mask
+	sll 	$t3,$t3,24
+	or 	$t1,$t1,$t3	# computer_bytes |= (network_bytes & byte_mask) << 24;
+
+	sll 	$t3,$t2,8	# t3 = byte_mask << 8
+	and 	$t3,$t0,$t3	# t3 = network_bytes & (byte_mask << 8)
+	sll 	$t3,$t3,8	# t3 = (network_bytes & (byte_mask << 8)) << 8
+	or 	$t1,$t1,$t3 	# computer_bytes |= (network_bytes & (byte_mask << 8)) << 8;
+
+	sll 	$t3,$t2,16	# t3 = byte_mask << 16
+	and 	$t3,$t0,$t3	# t3 = network_bytes & (byte_mask << 16)
+	srl 	$t3,$t3,8	# t3 = (network_bytes & (byte_mask << 16)) >> 8
+	or 	$t1,$t1,$t3 	# computer_bytes |= (network_bytes & (byte_mask << 16)) >> 8;
+
+	sll 	$t3,$t2,24	# t3 = byte_mask << 24
+	and 	$t3,$t0,$t3	# t3 = network_bytes & (byte_mask << 24)
+	srl 	$t3,$t3,24	# t3 = (network_bytes & (byte_mask << 24)) >> 24
+	or 	$t1,$t1,$t3 	# computer_bytes |= (network_bytes & (byte_mask << 24)) >> 24;
+
 
 	#
 	# Your code here!
 	#
 
-	move	$a0, $v0	# printf("%d\n", x);
+	move	$a0, $t1	# printf("%d\n", x);
 	li	$v0, 1
 	syscall
 
