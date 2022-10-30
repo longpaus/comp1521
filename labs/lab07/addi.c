d1e9c26b5602c93510afb0abfe084b797354c469
Hoang Long <longpaus@gmail.com>
2022-10-30 12:13:45 +1100

    did addi

--- labs/lab07/addi.c
+++ labs/lab07/addi.c
@@ -9,7 +9,22 @@
 
 // return the encoded binary MIPS for addi $t,$s, i
 uint32_t addi(int t, int s, int i) {
+    uint32_t fiveBitMask = (1 << 5) - 1; 
+    s = s & fiveBitMask; // get the first 5 bits of s
+    t = t & fiveBitMask; // get the first 5 bits of t
 
-    return 42; // REPLACE WITH YOUR CODE
+    uint32_t sixteenBitmask = (1 << 16) - 1;
+    i = i & sixteenBitmask; // get the first 16 bits of i
+
+    uint32_t ans = 8; // 001000
+    ans = ans << 5;
+    ans = ans | s; // place s into ans
+    ans = ans << 5;
+    ans = ans | t; // place t into ans
+    ans = ans << 16;
+    ans = ans | i;
+
+
+    return ans; 
 
 }
