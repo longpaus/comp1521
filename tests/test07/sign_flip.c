0e72fc6ff1ac67fe07097a0f67c41db2527ad7c7
Hoang Long <longpaus@gmail.com>
2022-11-02 22:01:47 +1100

    fix

--- tests/test07/sign_flip.c
+++ tests/test07/sign_flip.c
@@ -2,10 +2,10 @@
 
 // given the 32 bits of a float return it with its sign flipped
 uint32_t sign_flip(uint32_t f) {
-    uint32_t newNum = ((f >> 31) == 1) ? 0 : 1;
-    newNum <<= 31;
-    uint32_t mask = (1 << 31) - 1;
+    uint32_t newSign = ((f >> 31) == 1) ? 0 : 1;
+    newSign <<= 31;
+    uint32_t mask = ((uint32_t)1 << 31) - 1;
     f = f & mask;
-    return newNum | f;
+    return newSign | f;
 
 }
