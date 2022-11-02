af8cefb51f0ddc9c780047f7c65e5a7919ef5391
Hoang Long <longpaus@gmail.com>
2022-11-02 22:21:58 +1100

    fix

--- tests/test07/bit_rotate.c
+++ tests/test07/bit_rotate.c
@@ -4,11 +4,10 @@
 uint16_t bit_rotate(int n_rotations, uint16_t bits) {
     for(int i = 0; i < n_rotations; i++){
         //get last bit in bits
-        uint16_t lastBit = bits & 1;
-        
-        bits >>= 1;
-        lastBit <<= 15;
-        bits |= lastBit;
+        uint16_t firstBit = (bits >> 15);
+
+        bits <<= 1;
+        bits |= firstBit;
     }
     return bits; 
 }
