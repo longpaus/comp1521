a653ce3281fedcbe3e7438e8c75de48c6626194f
Hoang Long <longpaus@gmail.com>
2022-11-02 22:06:57 +1100

    did rotate

--- tests/test07/bit_rotate.c
+++ tests/test07/bit_rotate.c
@@ -2,5 +2,13 @@
 
 // return the value bits rotated left n_rotations
 uint16_t bit_rotate(int n_rotations, uint16_t bits) {
-    return 42; //REPLACE ME WITH YOUR CODE
+    for(int i = 0; i < n_rotations; i++){
+        //get last bit in bits
+        uint16_t lastBit = bits & 1;
+        
+        bits >>= 1;
+        lastBit <<= 15;
+        bits |= lastBit;
+    }
+    return bits; 
 }
