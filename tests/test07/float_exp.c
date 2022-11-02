5b4c1afd8ab2b6702fe9b266ad03e5187c02b702
Hoang Long <longpaus@gmail.com>
2022-11-02 21:41:12 +1100

    did float

--- tests/test07/float_exp.c
+++ tests/test07/float_exp.c
@@ -3,4 +3,7 @@
 // given the 32 bits of a float return the exponent
 uint32_t float_exp(uint32_t f) {
     return 42; // REPLACE ME WITH YOUR CODE
+    f >>= 23;
+    uint32_t mask = (1 << 8) - 1;
+    return f & mask;
 }
