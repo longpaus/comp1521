818a3dd90c4185be635d519e7affc10bab18e6f9
Hoang Long <longpaus@gmail.com>
2022-11-02 21:49:26 +1100

    did flip

--- tests/test07/sign_flip.c
+++ tests/test07/sign_flip.c
@@ -2,5 +2,10 @@
 
 // given the 32 bits of a float return it with its sign flipped
 uint32_t sign_flip(uint32_t f) {
-    return 42; // REPLACE ME WITH YOUR CODE
+    uint32_t newNum = ((f >> 30) == 1) ? 0 : 1;
+    newNum <<= 30;
+    uint32_t mask = (1 << 30) - 1;
+    f = f & mask;
+    return newNum | f;
+
 }
