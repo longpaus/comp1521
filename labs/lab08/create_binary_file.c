769f2b63e2649044003950407e173de8b8ab2950
Hoang Long <longpaus@gmail.com>
2022-11-06 15:33:22 +1100

    did

---
+++ labs/lab08/create_binary_file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    FILE *f = fopen(argv[1],"w");
+    for(int i = 1; i < argc; i++){
+        fputc(atoi(argv[i]),f);
+    }
+    fclose(f);
+
+}
--- labs/lab08/print_bytes.c
+++ labs/lab08/print_bytes.c
@@ -5,14 +5,12 @@ int main(int argc, char *argv[]){
     int c;
     int counter = 0;
     while((c = fgetc(f)) != EOF){
-        
-        if((c > 188 && c < 196) || c == '\t' || (c > 127 && c < 130) || (c >= 0 && c <= '\n')){
-            printf("byte %4d: %3d 0x%02x\n",counter,c,c);
+        if(c != '\n'){
+           printf("byte    %4d:  %3d 0x%02x '%c'\n",counter,c,c,c);
         }
-        else{
-            printf("byte %4d: %3d 0x%02x '%c'\n",counter,c,c,c);
+        else if(c == 10){
+            printf("byte    %4d:  %3d 0x%02x\n",counter,c,c);
         }
         counter++;
     }
-    fclose(f);
 }
