81fccb02db04ec550087300071f828a68718f9b4
Hoang Long <longpaus@gmail.com>
2022-11-06 12:41:42 +1100

    did

--- labs/lab08/create_integers_file.c
+++ labs/lab08/create_integers_file.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include <stdlib.h>
+
 
 int main(int argc, char *argv[]) { 
-    char mode[1] = "w";
+    char mode[10] = "w";
     FILE *f = fopen(argv[1],mode);
     int start = atoi(argv[2]);
     int end = atoi(argv[3]);
-    for(int i = start; i < end; i++){
-        fputc(i,f);
+    for(int i = start; i <= end; i++){
+        fprintf(f, "%d\n", i);
     }
     fclose(f);
 }
