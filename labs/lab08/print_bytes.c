c2e2146045f740be6611be629ebec6b4b3f2c90e
Hoang Long <longpaus@gmail.com>
2022-11-06 15:26:22 +1100

    done

--- labs/lab08/print_bytes.c
+++ labs/lab08/print_bytes.c
@@ -5,12 +5,14 @@ int main(int argc, char *argv[]){
     int c;
     int counter = 0;
     while((c = fgetc(f)) != EOF){
-        if(c != '\n'){
-           printf("byte    %4d:  %3d 0x%02x '%c'\n",counter,c,c,c);
+        
+        if((c > 188 && c < 196) || c == '\t' || (c > 127 && c < 130) || (c >= 0 && c <= '\n')){
+            printf("byte %4d: %3d 0x%02x\n",counter,c,c);
         }
-        else if(c == 10){
-            printf("byte    %4d:  %3d 0x%02x\n",counter,c,c);
+        else{
+            printf("byte %4d: %3d 0x%02x '%c'\n",counter,c,c,c);
         }
         counter++;
     }
+    fclose(f);
 }
