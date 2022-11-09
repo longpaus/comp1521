6f8d3211e9927951923e10f9298071c20a48abc8
Hoang Long <longpaus@gmail.com>
2022-11-09 12:23:36 +1100

    did no vowels

---
+++ tests/test08/remove_vowels.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool isLetterAVowel(char letter){
+	char vowels[10] = {'a','e','i','o','u','A','E','I','O','U'};
+	for(int i = 0;i< 10;i++){
+		if(vowels[i] == letter){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(int argc,char *argv[]){
+    FILE *f1 = fopen(argv[1],"r");
+    FILE *f2 = fopen(argv[2],"w");
+    int c;
+    while((c = fgetc(f1)) != EOF){
+        if(!isLetterAVowel(c)){
+            fputc(c,f2);
+        }
+    }
+    return 0;
+}
