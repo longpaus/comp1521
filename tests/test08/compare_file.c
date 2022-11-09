ffda008ffa80083fb07d86fc5551c41a6f1000cf
Hoang Long <longpaus@gmail.com>
2022-11-09 12:41:10 +1100

    done

--- tests/test08/compare_file.c
+++ tests/test08/compare_file.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-int main(int arc,char *argv){
+int main(int arc,char *argv[]){
     FILE *f1 = fopen(argv[1],"r");
     FILE *f2 = fopen(argv[2],"r");
-    int c1,c2,byte;
+    int c1,c2;
+    int byte = 0;
     while(1){
         c1 = fgetc(f1);
-        c2 = fegtc(f2);
+        c2 = fgetc(f2);
+        if(c1 == EOF && c2 == EOF){
+            printf("Files are identical\n");
+            break;
+        }
         if(c1 == EOF){
-            printf("EOF on %s",argv[1]);
+            printf("EOF on %s\n",argv[1]);
             break;
         }
         if(c2 == EOF){
-            printf("EOF on %s",argv[2]);
+            printf("EOF on %s\n",argv[2]);
             break;
         }
         if(c1 != c2){
-            printf("Files differ at byte %d",byte);
+            printf("Files differ at byte %d\n",byte);
+            break;
         }
         byte++;
     }
