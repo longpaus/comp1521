77a116dd5496611040329d6a5b79c35cb6b8b89c
Hoang Long <longpaus@gmail.com>
2022-11-22 13:47:04 +1100

    did

---
+++ labs/lab09/print_select_bytes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[]){
+    FILE *f = fopen(argv[1],"r");
+    int c;
+    for(int i = 2; i < argc; i++){
+        fseek(f,atoi(argv[i]),SEEK_SET);
+        c = fgetc(f);
+        printf("%d - 0x%02x - '%c'\n",c,c,c);
+    }
+    fclose(f);
+}
