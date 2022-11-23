33df6a742697ab4ed74963e33384f12399e643e9
Hoang Long <longpaus@gmail.com>
2022-11-23 16:34:10 +1100

    did q5

--- exam22t2final/22t2final_q5.c
+++ exam22t2final/22t2final_q5.c
@@ -3,5 +3,15 @@
 #include <stdio.h>
 
 void print_bytes(FILE *file, long n) {
-	// TODO
+	int max = (n >= 0) ? n : -n - 1;
+	int c;
+	int count = 0;
+	while((c = fgetc(file)) != EOF){
+		if(count > max){
+			break;
+		}
+		printf("%c",c);
+		count++;
+	}
+	fclose(file);
 }
