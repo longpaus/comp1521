a50eab9e3a55988df32bafc40b3b6c82e8134a19
Hoang Long <longpaus@gmail.com>
2022-11-28 17:21:23 +1100

    did q8

--- exam22t2final/22t2final_q8.s
+++ exam22t2final/22t2final_q8.s
@@ -9,12 +9,118 @@ default_char:	.byte ' '
 	.text
 # void move_disks(char size, char *target, char *peg1, char *peg2) {
 move_disks:
-	# TODO: Complete this function
+
+move_disks_prologue:
+	begin
+	push 	$ra
+	push 	$s0
+	push 	$s1
+	push 	$s2
+	push 	$s3
+	push 	$s4
+
+move_disks_body1:
+
+	la 	$s0,default_char		# s0 = peg_max
+	li 	$s1,0				# s1 = source
+	li 	$s2,0 				# s2 = other
+	move 	$s3,$a0				# s3 = size
+	move 	$s4,$a1				# s4 = target
+	b 	move_disks_for_init
+
+move_disks_for_init:
+	li 	$t0,0 				# t0 = i
+	b 	move_disks_for_cond
+move_disks_for_cond:
+	add 	$t1,$a2,$t0
+	lb 	$t1,($t1)			# t1 = peg1[i]
+	beq 	$t1,'\0',move_disks_body2
+	b 	move_disks_for_body1
+move_disks_for_body1:
+	ble 	$t1,$s0,move_disks_for_body2
+	bgt 	$t1,$s3,move_disks_for_body2
+	add 	$s0,$a2,$t0			# peg_max = peg1 + i;
+	move 	$s1,$a2				# source = peg1;
+	move 	$s2,$a3				# other = peg2;
+	b 	move_disks_for_body2
+
+move_disks_for_body2:
+	add 	$t1,$a3,$t0
+	lb 	$t1,($t1)			# t1 = peg2[i]
+
+	ble 	$t1,$s0,move_disks_for_iter
+	bgt 	$t1,$s3,move_disks_for_iter
+	add 	$s0,$a3,$t0			# peg_max = peg2 + i;
+	move 	$s1,$a3				# source = peg2;
+	move 	$s2,$a2				# other = peg1;
+
+	b 	move_disks_for_iter
+
+move_disks_for_iter:
+	addi 	$t0,$t0,1			# i++
+	b 	move_disks_for_cond
+
+move_disks_body2:
+	beq 	$s0,' ',move_disks_epiloque
+	addi 	$a0,$s3,-1
+	move 	$a1,$s2
+	move 	$a2,$s1
+	move 	$a3,$s4
+	jal 	move_disks
+
+	move 	$a0,$s4
+	jal 	find_lowest_target
+	move 	$a1,$v0
+	move 	$a0,$s0
+	jal 	swap
+	b 	move_disks_while_cond
+
+move_disks_while_cond:
+	addi 	$t0,$s0,1
+	beq 	$s0,$t0,move_disks_body3
+	b 	move_disks_while_body
+
+
+move_disks_while_body:
+	move 	$a0,$s0
+	addi 	$a1,$s0,1
+	jal 	swap
+	addi 	$s0,$s0,1			# peg_max++;
+	b 	move_disks_while_cond
+
+move_disks_body3:
+	jal 	print_towers
+	addi 	$a0,$s3,-1
+	move 	$a1,$s4
+	move 	$a2,$s1
+	move 	$a3,$s2
+	jal 	move_disks
+	b 	move_disks_epiloque
+move_disks_epiloque:
+	pop 	$ra
+	pop 	$s4
+	pop	$s3
+	pop	$s2
+	pop 	$s1
+	pop 	$s0
+	end
 	jr	$ra
 
 
 	.text
 # char *find_lowest_target(char *target) {
 find_lowest_target:
-	# TODO: Complete this function
+
+find_lowest_target_prologue:
+	push 	$ra
+
+find_lowest_target_body:
+	beq 	$a0,' ',find_lowest_target_epilogue
+	addi 	$a0,$a0,1
+	jal 	find_lowest_target
+
+find_lowest_target_epilogue:
+	pop 	$ra
+	end
+	move 	$v0,$a0
 	jr	$ra
--- test.c
+++ test.c
@@ -1,26 +1,22 @@
-void HeapInsert(Heap h, Item it)
-{
-   // is there space in the array?
-   assert(h->nitems < h->nslots);
-   h->nitems++;
-   // add new item at end of array
-   h->items[h->nitems] = it;
-   // move new item to its correct place
-   fixUp(h->items, h->nitems);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// force value at a[i] into correct position
-// note that N gives max index *and* # items
-void fixDown(Item a[], int i, int N)
-{
-   while (2*i <= N) {
-      // compute address of left child
-      int j = 2*i;
-      // choose larger of two children
-      if (j < N && less(a[j], a[j+1])) j++;
-      if (!less(a[i], a[j])) break;
-      swap(a, i, j);
-      // move one level down the heap
-      i = j;
-   }
+int main() {
+	struct stat s;
+	if (stat("test.c", &s) != 0) {
+		perror("test.c");
+		exit(1);
+	}
+	printf("ino =  %10ld # Inode number\n", s.st_ino);
+	printf("mode = %10o # File mode \n", s.st_mode);
+	printf("nlink =%10ld # Link count \n", (long)s.st_nlink);
+	printf("uid =  %10u # Owner uid\n", s.st_uid);
+	printf("gid =  %10u # Group gid\n", s.st_gid);
+	printf("size = %10ld # File size (bytes)\n", (long)s.st_size);
+
+	printf("mtime =%10ld # Modification time (seconds since 1/1/70)\n",
+	       (long)s.st_mtime);
 }
